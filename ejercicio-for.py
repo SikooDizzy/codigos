5b8f855191283dbbb658691ec67a552eea00ef94
SikooDizzy <6788bobynow2@gmail.com>
2026-06-23 15:36:04 -0400

    Refactor input handling for package weights

--- ejercicio-for.py
+++ ejercicio-for.py
@@ -1,6 +1,5 @@
 tienemasbultos = True 
 nrobulto = 1
-valorpagarporkilo = 0
 valorpesoliviano = 1000
 valorpesonormal = 4500
 totalliviano = 0
@@ -9,24 +8,29 @@ contadorbultoslivianos = 0
 contadorbultosnormales = 0
 
 cantidadbultos = int(input("Ingrese cantidad de bultos :"))
-for i in range (cantidadbultos):
-    try:
-        pesobulto = int(input(f"ingrese el peso (1 a 10kg) del bulto nro. {nrobulto}: "))
-    except ValueError:
-        print("Peso del bulto deben estar en el rango de 1 y 10kg. ")
-        continue
 
-    if 1 <= pesobulto <= 5:
-        totalliviano += valorpesoliviano
-        contadorbultoslivianos += 1
-    elif 6 <= pesobulto <= 10:
-        totalnormal += valorpesonormal
-        contadorbultosnormales += 1 
-    else:
-        print("Peso ingresado incorrecto (1 - 10kg)")
+for i in range(cantidadbultos):
+    while True:
+        try:
+            pesobulto = int(input(f"ingrese el peso (1 a 10kg) del bulto nro. {nrobulto}: "))
+            
+            if 1 <= pesobulto <= 5:
+                totalliviano += valorpesoliviano
+                contadorbultoslivianos += 1
+                break 
+            elif 6 <= pesobulto <= 10:
+                totalnormal += valorpesonormal
+                contadorbultosnormales += 1 
+                break 
+            else:
+                print("Peso ingresado incorrecto. Debe estar entre 1 y 10kg.")
+                
+        except ValueError:
+            print("Entrada inválida. Debe ingresar un número entero entre 1 y 10kg.")
 
     nrobulto += 1   
-print(f"Total a pagar por los bultos livianos: {totalliviano}")
+
+print(f"\nTotal a pagar por los bultos livianos: {totalliviano}")
 print(f"Total a pagar por los bultos normales: {totalnormal}")
 print(f"Cantidad de bultos livianos: {contadorbultoslivianos}")
 print(f"Cantidad de bultos normales: {contadorbultosnormales}")
